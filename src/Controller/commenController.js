744cc4abe512b8869e6227768936fa72e082a33d
Gustavo Martins <gustavo_mts2005@hotmail.com>
2023-02-01 20:33:52 -0300

    Fixing comment update redirection

--- src/Controller/commenController.js
+++ src/Controller/commenController.js
@@ -34,14 +34,13 @@ router.patch('/:movieId/critic/:criticId/comment/:commentId', async(req, res) =>
 
         if(!description)
             return res.status(400).send('Elementos PATCH não enviados!')
-        if(await (await Comments.find({$and: [{ _id: req.params.commentId}, {User: req.id }]})).length == 0) // testar
+        if((await Comments.find({$and: [{ _id: req.params.commentId}, {User: req.id }]})).length == 0) // testar
             return res.status(403).send('O usuário não possui permissão para alterar este comentário.')
 
         // set updatedAt with date now
         await Comments.updateOne({ _id: req.params.commentId }, { description: description })
 
-        res.send(await Comments.find({User: req.id }))
-        //res.redirect('http://localhost:3000/movie/' + req.params.movieId)
+        res.redirect('http://localhost:3000/movie/' + req.params.movieId)
     } catch(error) {
         return res.status(500).send(error)
     }
